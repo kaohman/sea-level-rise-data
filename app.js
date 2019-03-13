a27209fb9e0fff1db544850abc426d9c1be3c34e
Karin Ohman <kaohma@gmail.com>
2019-03-13 10:16:33 -0600

    Update api endpoints

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
+// get all states
 app.get('/api/v1/states', async (req, res) => {
   try {
     const states = await database('states').select();
@@ -17,41 +18,67 @@ app.get('/api/v1/states', async (req, res) => {
   }
 });
 
+// get all stations also query params
 app.get('/api/v1/stations', async (req, res) => {
+  const min = req.query.min;
+  const max = req.query.max;
+  const state_id = req.query.state_id;
+
   try {
-    const stations = await database('stations').select();
+    let stations = await database('stations').select();
+    if (state_id) stations = stations.filter(station => station.state_id === parseInt(state_id));
+    if (stations.length === 0) return res.status(404).json(`State id ${state_id} was not found.`)
+
+    if (min) stations = stations.filter(station => station.slr_rate >= min);
+    if (stations.length === 0) return res.status(404).json(`No records found greater than ${min}.`)
+
+    if (max) stations = stations.filter(station => station.slr_rate <= max);
+    if (stations.length === 0) return res.status(404).json(`No records found less than ${max}.`)
+
     return res.status(200).json(stations);
   } catch (error) {
     return res.status(500).json({ error })
   }
 });
 
-app.get('/api/v1/stations/:state_name', async (req, res) => {
-  const { state_name } = req.params;  
+// get a station by station id
+app.get('/api/v1/stations/:id', async (req, res) => {
+  const { id } = req.params;
   try {
-    const state = await database('states').select('id').where('name', state_name);
-    if (state.length === 0) return res.status(422).json(`State ${state_name} not found.`);
-    const stations = await database('stations').select().where('state_id', state[0].id);
+    const stations = await database('stations').select().where('id', id);
+    if (state.length === 0) return res.status(404).json(`Station id ${id} not found.`);
     return res.status(200).json(stations);
   } catch (error) {
     return res.status(500).json({ error })
   }
 });
 
-// get all stations given a slr greater than or less than parameter
-  // /api/v1/stations?greater_than=3
+// get a state by state id
+app.get('/api/v1/states/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const states = await database('states').select().where('id', id);
+    if (state.length === 0) return res.status(404).json(`State id ${id} not found.`);
+    return res.status(200).json(states);
+  } catch (error) {
+    return res.status(500).json({ error })
+  }
+});
 
-app.post('/api/v1/stations/new/:state_name', async (req, res) => {
-  const { noaa_id, name, slr_rate } = req.body;
-  const { state_name } = req.params;
-  for (let requiredParameter of ['noaa_id', 'name', 'slr_rate']) {
+// post a new station
+app.post('/api/v1/stations', async (req, res) => {
+  const { noaa_id, name, slr_rate, state_id } = req.body;
+  for (let requiredParameter of ['noaa_id', 'name', 'slr_rate', 'state_id']) {
     if (!req.body[requiredParameter]) return res.status(422).json(`Expected format: { name: <String>, noaa_id: <Integer>, slr_rate: <Float> }. You are missing a ${requiredParameter}`);
   }
 
   try {
     const state = await database('states').select('id').where('name', state_name);
-    if (state.length === 0) return res.status(404).json(`State ${state_name} not found.`);
-    // check db to see if station already exists?
+    if (state.length === 0) return res.status(404).json(`State id ${state_id} not found.`)
+
+    const dupStation = await database('stations').select().where('noaa_id', noaa_id);
+    if (dupStation.length > 0) return res.status(409).json(`Conflict. NOAA tide station id ${noaa_id} already exists`)
+
     const stationId = await database('stations').insert({
       state_id: state[0].id,
       noaa_id,
@@ -65,14 +92,16 @@ app.post('/api/v1/stations/new/:state_name', async (req, res) => {
 
 });
 
-app.post('/api/v1/states/new', async (req, res) => {
+// post a new state
+app.post('/api/v1/states', async (req, res) => {
   const { name, coast } = req.body;
   for (let requiredParameter of ['name', 'coast']) {
     if (!req.body[requiredParameter]) return res.status(422).json(`Expected format: { name: <String>, noaa_id: <Integer>, slr_rate: <Float> }. You arer missing a ${requiredParameter}`);
   }
 
   try {
-    // check db to see if state already exists?
+    const dupStation = await database('states').select().where('name', name);
+    if (dupStation.length > 0) return res.status(409).json(`Conflict. State name ${name} already exists`)
     const stateId = await database('states').insert({
       name,
       coast
@@ -83,30 +112,17 @@ app.post('/api/v1/states/new', async (req, res) => {
   }
 });
 
-app.delete('/api/v1/states/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    await database('states').where('id', id).del();
-    return res.sendStatus(204)
-  } catch (error) {
-    return res.status(500).json({ error })
-  }
-});
-
+// delete a station
 app.delete('/api/v1/stations/:id', async (req, res) => {
   const { id } = req.params;
   try {
+    const matchingStation = database('stations').select().where('id', id);
+    if (matchingStation.length === 0) return res.status(404).json(`Station id ${station} not found.`)
     await database('stations').where('id', id).del();
     return res.sendStatus(204)
   } catch (error) {
     return res.status(500).json({ error })
   }
 });
-
-
-// EXTRAS
-
-// get an individual station with the noaa id
-// get all stations for a given coast
 
 module.exports = app;
