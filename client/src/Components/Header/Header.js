e018c1f7cca1fa46f5d22b23d58fa53eb2b43b79
Karin Ohman <kaohma@gmail.com>
2019-03-31 14:46:54 -0600

    Update title

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header id='top'>
-      <h1>NOAA Sea Level Rise API</h1>
+      <h1>NOAA Sea Level Trends API</h1>
       <h4>This REST API contains sea level trend data for National Oceanic and Atmospheric Administration (NOAA) tide stations across the United States. Details about the NOAA data can be found <a href='https://tidesandcurrents.noaa.gov/sltrends/sltrends_us.html'>HERE</a>. Users can access data for U.S. States and tide stations from the database, as well as post additional states and stations and delete stations.</h4>
       <h2>Requests</h2>
       <h4 id='base-text'>The base URL for all requests is:</h4>
